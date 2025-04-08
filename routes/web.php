5945b2072d563930f9ef416157f73158ac0dab51
Barac Fabregat <barac.fabregat.gallegos@tuta.com>
2025-04-08 00:40:30 -0600

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,41 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\SaleStockController;
+use App\Http\Controllers\ShoppingCartProductController;
+use App\Http\Controllers\StockController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:sanctum')->group(function () {
+    // CategoryController
+    Route::get('categories', [CategoryController::class, 'index']);
+
+    // ProductController
+    Route::get('products', [ProductController::class, 'index']);
+    Route::post('products', [ProductController::class, 'store']);
+    Route::patch('products', [ProductController::class, 'update']);
+    Route::delete('products', [ProductController::class, 'switchActiveStatus']);
+
+    // StockController
+    Route::get('stocks', [StockController::class, 'index']);
+    Route::post('stocks', [StockController::class, 'store']);
+    Route::patch('stocks', [StockController::class, 'update']);
+
+    // SaleController
+    Route::get('sale', [SaleController::class, 'index']);
+    Route::post('sale', [SaleController::class, 'store']);
+
+    // SaleStockController
+    Route::get('sale_stock', [SaleStockController::class, 'index']);
+    Route::post('sale_stock', [SaleStockController::class, 'store']);
+
+    // ShoppingCartProductController
+    Route::get('shopping_cart_product', [ShoppingCartProductController::class, 'index']);
+    Route::post('shopping_cart_product', [ShoppingCartProductController::class, 'store']);
+    Route::patch('shopping_cart_product', [ShoppingCartProductController::class, 'update']);
+    Route::delete('shopping_cart_product', [ShoppingCartProductController::class, 'destroy']);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
